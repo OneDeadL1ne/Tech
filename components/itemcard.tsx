eb3fb494f7703001cdc8ced14e8401acacf7dd52
Yuka <maxzerovol@gmail.com>
2023-10-13 16:40:13 +0300

    up

--- components/itemcard.tsx
+++ components/itemcard.tsx
@@ -2,15 +2,13 @@
 
 import Image from "next/image";
 import BasicRating from "./rating";
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
-import { useWindowSize } from "@/hook/useWindowSize";
+import { YMaps, Map } from "@pbe/react-yandex-maps";
 
 export function Item() {
-  const { height, width } = useWindowSize();
   return (
     <div>
-      <div className="grid   relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-white">
-        <div className="grid   p-5">
+      <div className="grid  md:grid-cols-2 relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-white">
+        <div className="grid md:grid-rows-4  p-5">
           <div className="">
             <div className="text-lg md:text-2xl">
               Дополнительное отделение центрального офиса.
@@ -44,23 +42,14 @@ export function Item() {
             <div className="text-xs md:text-base text-[#848484]">Шоссе</div>
           </div>
         </div>
-        {width > 1050 && (
-          <div className="flex justify-center  p-5">
-            <YMaps>
-              <Map
-                className="h-full w-full rounded-md"
-                defaultState={{ center: [55.758798, 37.75221], zoom: 15 }}
-              >
-                <Placemark
-                  geometry={[55.758798, 37.75221]}
-                  options={{
-                    iconImageHref: "",
-                  }}
-                />
-              </Map>
-            </YMaps>
-          </div>
-        )}
+        <div className=" p-5">
+          <YMaps >
+            <Map
+              className="h-full w-full rounded-full block"
+              defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+            ></Map>
+          </YMaps>
+        </div>
       </div>
     </div>
   );
