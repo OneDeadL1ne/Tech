b730bb95ccc07641e280f8006f35287b852b98df
Yuka <maxzerovol@gmail.com>
2023-10-13 16:13:03 +0300

    wf

--- components/itemcard.tsx
+++ components/itemcard.tsx
@@ -7,7 +7,7 @@ export function Item() {
       <div className="grid  md:grid-cols-2 relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-white">
         <div className="grid md:grid-rows-4  p-5">
           <div className="">
-            <div className="text-2xl">
+            <div className="text-lg md:text-2xl">
               Дополнительное отделение центрального офиса.
             </div>
             <div>
@@ -16,23 +16,23 @@ export function Item() {
           </div>
 
           <div>
-          <div className="text-lg inline-block">Обслуживание</div>
+          <div className="text-sm md:text-lg inline-block">Обслуживание</div>
           <div className="inline-flex m-1"><Image src="/dopinf.png" alt="" width={10} height={10}/></div>
             <div className="text-[#848484]">
-              <div className="">Физ лица</div>
-              <div>Юр лица</div>
+              <div className="text-xs md:text-base">Физ лица</div>
+              <div className="text-xs md:text-base">Юр лица</div>
             </div>
           </div>
           <div>
-            <div className="text-lg inline-block">Режим</div>
+            <div className="text-sm md:text-lg inline-block">Режим</div>
             <div className="inline-flex m-1"><Image src="/dopinf.png" alt="" width={10} height={10}/></div>
-            <div className="text-[#848484]">10 00</div>
+            <div className="text-xs md:text-base text-[#848484]">10 00</div>
           </div>
 
           <div>
-            <div className="text-lg">Адрес</div>
-            <div className="text-[#848484]">Шоссе</div>
-            <div className="text-[#848484]">Шоссе</div>
+            <div className="text-sm md:text-lg">Адрес</div>
+            <div className="text-xs md:text-base text-[#848484]">Шоссе</div>
+            <div className="text-xs md:text-base text-[#848484]">Шоссе</div>
           </div>
         </div>
         <div className="flex justify-center p-5">
