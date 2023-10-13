4f7d3cd69614314ae843f3028ba6b937562f11a9
Yuka <maxzerovol@gmail.com>
2023-10-13 16:03:55 +0300

    up

--- components/itemcard.tsx
+++ components/itemcard.tsx
@@ -4,10 +4,10 @@ import BasicRating from "./rating";
 export function Item() {
   return (
     <div>
-      <div className="grid grid-cols-2 relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-white">
-        <div className="grid grid-rows-4 p-5">
+      <div className="grid  md:grid-cols-2 relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-white">
+        <div className="grid md:grid-rows-4  p-5">
           <div className="">
-            <div className="text-4xl">
+            <div className="text-2xl">
               Дополнительное отделение центрального офиса.
             </div>
             <div>
@@ -16,25 +16,29 @@ export function Item() {
           </div>
 
           <div>
-            <div className="text-xl">Обслуживание</div>
-            <div className="grid grid-cols-2 text-gray-500">
-              <div>Физ лица</div>
+          <div className="text-lg inline-block">Обслуживание</div>
+          <div className="inline-flex m-1"><Image src="/dopinf.png" alt="" width={10} height={10}/></div>
+            <div className="text-[#848484]">
+              <div className="">Физ лица</div>
               <div>Юр лица</div>
             </div>
           </div>
           <div>
-            <div className="">Режим</div>
-            <div>10 00</div>
+            <div className="text-lg inline-block">Режим</div>
+            <div className="inline-flex m-1"><Image src="/dopinf.png" alt="" width={10} height={10}/></div>
+            <div className="text-[#848484]">10 00</div>
           </div>
 
           <div>
-            <div className="">Адрес</div>
-            <div>Шоссе</div>
-            <div>Шоссе</div>
+            <div className="text-lg inline-block">Адрес</div>
+            <div className="inline-flex m-1"><Image src="/dopinf.png" alt="" width={10} height={10}/></div>
+            <div className="text-[#848484]">Шоссе</div>
+            <div className="text-[#848484]">Шоссе</div>
           </div>
         </div>
         <div className="flex justify-center p-5">
-          <Image src="/test.png" alt="" width={400} height={400} />
+          <Image src="/test.png" alt="" width={300} height={100} />
+          
         </div>
       </div>
     </div>
