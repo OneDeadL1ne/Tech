7675ade9795459a5f6730cdd4d59b9db89a260b1
Yuka <maxzerovol@gmail.com>
2023-10-13 23:24:26 +0300

    crash switch

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,6 +2,7 @@ import { ParametrsMenu } from "./Parametrs";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { Input } from "./ui/input";
+import { Switch } from "./ui/switch";
 
 export function Search() {
   return (
@@ -23,22 +24,136 @@ export function Search() {
                 Найти
               </Button>
             </div>
-            <div className="text-white">
-              <div className="mt-8">
-                Метро
-                <Input className=" top-0 max-w-[50%] text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px]" />
-              </div>
-              <div className="mt-8">
-                Расстояние
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="terms" />
-                  <label
-                    htmlFor="terms"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    1 Км
-                  </label>
+            <div className="grid grid-cols-2">
+              <div className="text-white pr-[5%]">
+                <div className="mt-8">
+                  Метро
+                  <Input className=" top-0 text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px] mt-4" />
                 </div>
+                <div className="mt-8">
+                  Расстояние
+                  <div className="grid grid-cols-4 mt-5">
+                    <div className="flex items-center space-x-2">
+                      <Checkbox id="terms" />
+                      <label
+                        htmlFor="terms"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        1 Км
+                      </label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <Checkbox id="terms" />
+                      <label
+                        htmlFor="terms"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        5Км
+                      </label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <Checkbox id="terms" />
+                      <label
+                        htmlFor="terms"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        20 Км
+                      </label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <Checkbox id="terms" />
+                      <label
+                        htmlFor="terms"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                    > 100 Км
+                      </label>
+                    </div>
+                  </div>
+                  <div className="mt-12">
+                    Время Работы
+                    <div className="flex justify-center mt-4">
+                      <Input className=" top-0 text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px] max-w-[20%] mr-[10%]" />
+                      <Input className=" top-0 text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px] max-w-[20%] ml-[10%]" />
+                    </div>
+                    <div className="flex justify-center mt-4">
+                      <div className="flex items-center space-x-2  mr-[10%]">
+                        <Checkbox id="terms" />
+                        <label
+                          htmlFor="terms"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                    > 100 Км
+                        </label>
+                      </div>
+                      <div className="flex items-center space-x-2 ml-[10%]">
+                        <Checkbox id="terms" />
+                        <label
+                          htmlFor="terms"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                    > 100 Км
+                        </label>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+              <div className="text-white pl-[5%]">
+                <div className="mt-8">
+                  Город
+                  <Input className=" top-0 text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px] mt-4" />
+                </div>
+                <div className="mt-8">
+                  Тип Отделения
+                  <Input className=" top-0 text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px] mt-4" />
+                </div>
+                <div className="mt-8">
+                  <div>
+                    Банкомат
+                  </div>
+                  <div className="grid grid-cols-2">
+                    <div>
+                      Валюта
+                      <div className="flex items-center space-x-2 mt-4">
+                        <Checkbox id="terms" />
+                        <label
+                          htmlFor="terms"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                    $
+                        </label>
+                      </div>
+                      <div className="flex items-center space-x-2 mt-4">
+                        <Checkbox id="terms" />
+                        <label
+                          htmlFor="terms"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                    €
+                        </label>
+                      </div>
+                      <div className="flex items-center space-x-2 mt-4">
+                        <Checkbox id="terms" />
+                        <label
+                          htmlFor="terms"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                    ₽
+                        </label>
+                      </div>
+                    </div>
+                    <div>
+                      Снять/внести
+                      <div>
+                        <Switch id="qwe"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
               </div>
             </div>
           </form>
