e0c13fac4ee2c39f552a21a13b253439e645f464
OneDeadL1ne <mikemp201@gmail.com>
2023-10-13 16:04:12 +0300

    up parametr

--- components/Search.tsx
+++ components/Search.tsx
@@ -9,23 +9,21 @@ export function Search() {
         <div className="bg-[#ffffff1b] bg-gradient-to-r from-[#ffffff25] p-5 rounded-[20px]">
           <form>
             <div>
+              <Input className=" top-0 right-0 text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px]" />
+              <div className="absolute top-5 right-24">
+                <ParametrsMenu />
+              </div>
 
-            <Input className=" top-0 right-0 text-white border-[#fff0] bg-[#ffffff45] h-[40px]  rounded-[20px]" />
-            <div className="absolute top-5 right-24">
-            <ParametrsMenu />
-            </div>
-            
-            <Button
-              type="submit"
-              variant="default"
-              className="absolute bg-[ffffff] hover:bg-[ffffff] top-5 right-8 text-white border-[#fff0]"
-            >
-              Найти
-            </Button>
+              <Button
+                type="submit"
+                variant="default"
+                className="absolute bg-[ffffff] hover:bg-[ffffff] top-5 right-8 text-white border-[#fff0]"
+              >
+                Найти
+              </Button>
             </div>
           </form>
         </div>
-        
       </div>
     </div>
   );
