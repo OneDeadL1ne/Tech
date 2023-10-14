792621b01a12dfeb93b749e5899d57be25594fd2
Yuka <maxzerovol@gmail.com>
2023-10-15 01:05:25 +0300

    chat

--- components/chat.tsx
+++ components/chat.tsx
@@ -10,6 +10,12 @@ export function Chat() {
   return (
     <div>
       <div className="min-h-[500px] relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-black">
+      <Button
+        type="submit"
+        className=" text-white font-bold py-2 px-4 rounded-[10px] "
+      >
+        x
+      </Button>
        <div className="p-3">
         <div className="flex justify-end">
     <div className="  bg-[#00AAFF] rounded-[10px] max-w-[300px] p-2 text-white">
@@ -26,25 +32,25 @@ wefewwevewvewv
         <div className="grid grid-cols-4 mb-3 place-items-center">
         <Button
         type="submit"
-        className="bg-[#97979778] text-white font-bold rounded-[10px] max-w-[200px]"
+        className="bg-[#97979778] hover:bg-[#979797cc] text-white font-bold rounded-[10px] max-w-[200px]"
       >
         Применить
       </Button>
       <Button
         type="submit"
-        className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] "
+        className="bg-[#97979778] hover:bg-[#979797cc] text-white font-bold py-2 px-4 rounded-[10px] "
       >
         Применить
       </Button>
       <Button
         type="submit"
-        className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] "
+        className="bg-[#97979778] hover:bg-[#979797cc] text-white font-bold py-2 px-4 rounded-[10px] "
       >
         Применить
       </Button>
       <Button
         type="submit"
-        className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] "
+        className="bg-[#97979778] hover:bg-[#979797cc] text-white font-bold py-2 px-4 rounded-[10px] "
       >
         Применить
       </Button>
