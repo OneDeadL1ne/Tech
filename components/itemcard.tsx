8460b2b3a06b4210d23ad26fb0947b75cf9c194b
Yuka <maxzerovol@gmail.com>
2023-10-15 01:19:50 +0300

    qwe

--- components/itemcard.tsx
+++ components/itemcard.tsx
@@ -5,6 +5,7 @@ import BasicRating from "./rating";
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 import { useWindowSize } from "@/hook/useWindowSize";
 import { WorkTime } from "./WorkTime";
+import { InputPro } from "./InputPro";
 
 export function Item() {
   const { height, width } = useWindowSize();
@@ -60,6 +61,14 @@ export function Item() {
             </YMaps>
           </div>
         )}
+      </div>
+      <div className="grid  md:grid-cols-2 relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%]  lg:grid-cols-2 text-white">
+        <div className="grid grid-cols-2">
+        <InputPro/>
+        <div>
+          Вернуться в чат
+        </div>
+        </div>
       </div>
     </div>
   );
