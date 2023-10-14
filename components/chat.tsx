010af78431b8073abc9a2ed4933c068d6ff692e8
OneDeadL1ne <mikemp201@gmail.com>
2023-10-15 02:30:14 +0300

    попытка добавить скрол

--- components/chat.tsx
+++ components/chat.tsx
@@ -5,7 +5,14 @@ import BasicRating from "./rating";
 import { YMaps, Map } from "@pbe/react-yandex-maps";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FormEvent, useState } from "react";
+import {
+  FormEvent,
+  MutableRefObject,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { useChatScroll } from "@/hook/useScroll";
 
 type Data = {
   author: string;
@@ -25,14 +32,25 @@ export function Chat() {
     setText("");
   }
 
+  const messageEl = useRef(null);
+
+  useEffect(() => {
+    if (messageEl) {
+      messageEl.current.addEventListener("DOMNodeInserted", (event: any) => {
+        const { currentTarget: target } = event;
+        target.scroll({ top: target.scrollHeight, behavior: "smooth" });
+      });
+    }
+  }, []);
+
   return (
     <div>
       <div className="min-h-[500px] relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-black">
-        <div className="p-3">
+        <div className="p-3" ref={scrollTargetRef}>
           {messages.map((mes) => (
             <div>
               {mes.author === "user" ? (
-                <div className="flex justify-end">
+                <div className="flex justify-end mt-2">
                   <div className="  bg-[#00AAFF] rounded-[10px] max-w-[300px] p-2 text-white">
                     {mes.message}
                   </div>
@@ -47,24 +65,16 @@ export function Chat() {
             </div>
           ))}
         </div>
+
         <div className="absolute bottom-0 w-[100%] p-3 grid grid-rows-2">
           <div className="grid grid-cols-4 mb-3 place-items-center">
-            <Button
-              type="submit"
-              className="bg-[#97979778] text-white font-bold rounded-[10px] max-w-[200px]"
-            >
+            <Button className="bg-[#97979778] text-white font-bold rounded-[10px] max-w-[200px]">
               Применить
             </Button>
-            <Button
-              type="submit"
-              className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] "
-            >
+            <Button className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] ">
               Применить
             </Button>
-            <Button
-              type="submit"
-              className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] "
-            >
+            <Button className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] ">
               Применить
             </Button>
             <Button className="bg-[#97979778] text-white font-bold py-2 px-4 rounded-[10px] ">
