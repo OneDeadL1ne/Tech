76559d3a4a77990e674b5547961c9f024785aee7
Yuka <maxzerovol@gmail.com>
2023-10-14 23:45:16 +0300

    efw

--- components/chat.tsx
+++ components/chat.tsx
@@ -9,18 +9,18 @@ import { Button } from "@/components/ui/button";
 export function Chat() {
   return (
     <div>
-      <div className="min-h-[500px] relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-white">
+      <div className="min-h-[500px] relative ml-[10%] mr-[10%] min-[420px]:ml-[15%] min-[420px]:mr-[15%] bg-[#ffffff0f] bg-gradient-to-r from-[#ffffff3d] rounded-xl  lg:grid-cols-2 text-black">
        <div>
 
        </div>
        <div className="absolute bottom-0 w-[100%] p-3">
-       <Button
+        <Input className="rounded-[10px] bg-white"/>
+        <Button
         type="submit"
-        className="bg-[#ffffff45] hover:bg-[#a1fda16b] text-white font-bold py-2 px-4 rounded-full absolute bottom-0 right-0"
+        className="bg-[#323232] hover:bg-[#a1fda16b] text-white font-bold py-2 px-4 rounded-full absolute bottom-0 right-0"
       >
         Применить
       </Button>
-        <Input className="rounded-[10px] bg-white"/>
        </div>
       </div>
     </div>
